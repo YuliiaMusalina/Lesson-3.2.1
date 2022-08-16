ffb1acc724a26fd67082ecccd24b6534d275e85a
Ulia Kamalova <mysula@mail.ru>
2022-08-16 16:43:31 +0500

    10 ЗАДАЧА

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿int randomNumber = GetRandomNumber(100, 999);
+Console.WriteLine(randomNumber);
+
+int firstDigit = randomNumber / 100;
+int secondDigit = randomNumber / 10;
+int thirdDigit = randomNumber % 10;
+
+Console.WriteLine(secondDigit % 10);
+
+
+int GetRandomNumber(int min, int max)
+{
+    return new Random().Next(min, max + 1);
+}
+
